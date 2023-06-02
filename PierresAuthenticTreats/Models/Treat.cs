00ac17114a850128bb4504899f744d8e772abd91
Emma Gerigscott <elgerig@gmail.com>
2023-06-02 16:05:35 -0700

    add validation for treat name

--- PierresAuthenticTreats/Models/Treat.cs
+++ PierresAuthenticTreats/Models/Treat.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PierresAuthenticTreats.Models
 {
   public class Treat
   {
     public int TreatId { get; set; }
+    [Required(ErrorMessage = "Treat must have a name!")]
     public string Name { get; set; }
     public float Price { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
