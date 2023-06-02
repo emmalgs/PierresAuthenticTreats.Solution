628db0382fe54941a899514d8230adb586781f60
Emma Gerigscott <elgerig@gmail.com>
2023-06-02 10:47:12 -0700

    update db for user field in treats and flavors

---
+++ PierresAuthenticTreats/Migrations/20230602174631_UserPropertyToTreatsAndFlavors.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierresAuthenticTreats.Migrations
+{
+    public partial class UserPropertyToTreatsAndFlavors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Treats",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Flavors",
+                keyColumn: "Type",
+                keyValue: null,
+                column: "Type",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Flavors",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Flavors",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Treats_AspNetUsers_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Treats_UserId",
+                table: "Treats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Treats");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Flavors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Flavors",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
