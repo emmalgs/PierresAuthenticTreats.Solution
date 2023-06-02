af2eccce9d92d4c8c2f9870032bd6596877a5fba
Emma Gerigscott <elgerig@gmail.com>
2023-06-02 11:37:10 -0700

    add new migration

--- PierresAuthenticTreats/Migrations/20230602183340_RemoveUserPropertyFromFlavors.Designer.cs
+++ PierresAuthenticTreats/Migrations/20230602183340_RemoveUserPropertyFromFlavors.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PierresAuthenticTreats.Models;
 
@@ -10,9 +11,10 @@ using PierresAuthenticTreats.Models;
 namespace PierresAuthenticTreats.Migrations
 {
     [DbContext(typeof(PierreContext))]
-    partial class PierreContextModelSnapshot : ModelSnapshot
+    [Migration("20230602183340_RemoveUserPropertyFromFlavors")]
+    partial class RemoveUserPropertyFromFlavors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
