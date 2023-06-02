63cbf6f9218d94cb361b01ae6c79821dd5c48664
Emma Gerigscott <elgerig@gmail.com>
2023-06-02 15:29:49 -0700

    update appuser to have ordertotal

---
+++ PierresAuthenticTreats/Migrations/20230602222859_AddOrderTotalPropertyToAppUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierresAuthenticTreats.Migrations
+{
+    public partial class AddOrderTotalPropertyToAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "OrderTotal",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderTotal",
+                table: "AspNetUsers");
+        }
+    }
+}
