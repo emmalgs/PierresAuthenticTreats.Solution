5fa6afc316d8f5962aa14a41b1ae424e50e07dbf
Emma Gerigscott <elgerig@gmail.com>
2023-06-02 08:55:02 -0700

    add treat class

---
+++ PierresAuthenticTreats/Models/Treat.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace PierresAuthenticTreats.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public List<FlavorTreat> JoinEntities { get; }
+  }
+}
